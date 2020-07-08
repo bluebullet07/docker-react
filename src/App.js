c879347ad464868092292b747f4e59d87c76818a
bluebullet07 <brain.k7@gmail.com>
2020-07-08 22:18:27 +0100

    modified app.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there welcome to react !
+          Hello mate Welcome to react !
         </p>
         <a
           className="App-link"
